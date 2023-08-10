046f989fa1e32fcaf6191a554abfa4db565cff14
MeuhMeuh62 <86471514+MeuhMeuh62@users.noreply.github.com>
2023-08-10 16:35:27 +0200

    update

--- H4KING-ZONE/code/bat/fichiers/folder-bomb.bat
+++ H4KING-ZONE/code/bat/fichiers/folder-bomb.bat
@@ -1,5 +1,22 @@
 @echo off
-title Folder bomb
+title WARNING - Folder bomb
+echo --------------------------------------------------------------------------
+echo .
+echo AFTER START THIS PROGRAM MAKE SURE YOU RUN IT INTO THE FOLDER IN QUESTION.
+echo IF IS NOT, CLOSE THE PROGRAM AND MOVE IT.
+echo IF YOU CAN'T MOVE THE PROGRAM, CHANGE THE DIRECTORY INTO THE FILE LINE 22.
+echo For security, another pause will be comming after this.
+echo .
+echo --------------------------------------------------------------------------
+pause
+echo ---------------------------------------------
+echo .
+echo THE PROGRAM WILL BE START AFTER THIS PAUSE.
+echo IL FOU WANT TO STOP THE PROGRAM DO : CTRL + C
+echo .
+echo ---------------------------------------------
+pause
+title IN PROGRESS - Folder bomb
 color 2
 :A
 md %random%
